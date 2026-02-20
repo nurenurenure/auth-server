87380b529b0a0f079f80f84994cf1b33b4d06e87
Anna Muraveva <tomuchnure@gmail.com>
2026-02-20 20:28:07 +0700

    logger added

--- api/cmd/sso/main.go
+++ api/cmd/sso/main.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	"api/internal/config"
-	"fmt"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
 
+	log.Info("starting application", slog.String("env", cfg.Env))
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+
+	}
+
+	return log
 }
