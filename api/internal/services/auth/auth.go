250370e407567192033e0599cb5b77210353c139
Anna Muraveva <tomuchnure@gmail.com>
2026-03-19 21:52:44 +0700

    Login() added

--- api/internal/services/auth/auth.go
+++ api/internal/services/auth/auth.go
@@ -2,9 +2,12 @@ package auth
 
 import (
 	"context"
+	"fmt"
 	"gRPC/api/internal/domain/models"
 	"log/slog"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Auth struct {
@@ -55,7 +58,25 @@ func (a *Auth) Login(
 	panic("not implemented")
 }
 func (a *Auth) RegisterNewUser(ctx context.Context, email string, pass string) (int64, error) {
-	panic("not implemented")
+	const op = "auth.RegisterNewUser"
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email),
+	)
+	log.Info("registering user")
+	passHash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to generate password hash", slog.String("error", err.Error()))
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	id, err := a.userSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to saave user", slog.String("error", err.Error()))
+		return 0, fmt.Errorf("%s: %w", op, err)
+
+	}
+	log.Info("user registered")
+	return id, nil
 }
 func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
 	panic("not implemented")
--- go.mod
+++ go.mod
@@ -11,9 +11,10 @@ require (
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/net v0.48.0 // indirect
-	golang.org/x/sys v0.39.0 // indirect
-	golang.org/x/text v0.32.0 // indirect
+	golang.org/x/crypto v0.49.0 // indirect
+	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/sys v0.42.0 // indirect
+	golang.org/x/text v0.35.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
